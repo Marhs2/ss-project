158626f0640b08c6965af5b2637174fc8176c324
Marhs2 <wjdehddfl12@gmail.com>
2024-07-08 18:53:37 +0900

    채팅

--- 채팅/chat.js
+++ 채팅/chat.js
@@ -9,20 +9,8 @@ $(function(){
         $("#box").append(tr);
         $("#input").val("");
 
-        //var maxScroll = $("#box").height() - $("#topbox").height();
-        //$("#topbox").scrollTop(maxScroll);
         })
 
-    $("#input").on("keyup",function(e){
-        if(e.keyCode == 13){
-            var msg = $("#input").val();
-            var tr = $("<tr></tr>");
-            tr.append("<td>"+msg)
-            $("#input").val("");
-
-        }
-    })
 
 
-
-})
+})  
